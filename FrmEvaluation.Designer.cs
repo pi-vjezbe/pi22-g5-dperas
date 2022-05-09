6066f97faf0b1a5e322ac692c6352c74309d2aee
PI <pi_foi@outlook.com>
2022-05-09 19:22:03 +0200

    Implementiran odabira aktivnosti na formi za evaluaciju.

--- FrmEvaluation.Designer.cs
+++ FrmEvaluation.Designer.cs
@@ -84,6 +84,7 @@
 			this.cboActivities.Name = "cboActivities";
 			this.cboActivities.Size = new System.Drawing.Size(277, 21);
 			this.cboActivities.TabIndex = 3;
+			this.cboActivities.SelectedIndexChanged += new System.EventHandler(this.cboActivities_SelectedIndexChanged);
 			// 
 			// txtMinForSignature
 			// 
@@ -182,6 +183,7 @@
 			this.btnCancel.TabIndex = 14;
 			this.btnCancel.Text = "Odustani";
 			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
 			// 
 			// FrmEvaluation
 			// 
@@ -205,6 +207,7 @@
 			this.Controls.Add(this.label1);
 			this.Name = "FrmEvaluation";
 			this.Text = "FrmEvaluation";
+			this.Load += new System.EventHandler(this.FrmEvaluation_Load);
 			((System.ComponentModel.ISupportInitialize)(this.numPoints)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
